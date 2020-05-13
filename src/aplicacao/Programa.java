d4142ee608579f6f2b5c4c99e6d5e56641201f76
ElianMelo <elianmelomorais@gmail.com>
2020-05-13 19:27:43 -0300

    Leitura da escolaridade

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,6 +10,17 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int escol;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		
+		escol = sc.nextInt();
+		
 		sc.close();
 
 	}
